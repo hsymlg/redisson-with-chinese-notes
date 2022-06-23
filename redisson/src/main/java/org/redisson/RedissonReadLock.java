296fbfeabac099ee2311256184939b6423fdb228
lingao <1249392391@qq.com>
2022-06-23 10:39:35 +0800

    lock

--- redisson/src/main/java/org/redisson/RedissonReadLock.java
+++ redisson/src/main/java/org/redisson/RedissonReadLock.java
@@ -136,45 +136,74 @@ public class RedissonReadLock extends RedissonLock implements RLock {
         String keyPrefix = getKeyPrefix(threadId, timeoutPrefix);
 
         return evalWriteAsync(getRawName(), LongCodec.INSTANCE, RedisCommands.EVAL_BOOLEAN,
+                //分支一：锁模式不存在，往锁对应的channel发送消息
+                //如果锁模式不存在，那么证明没有线程持有读写锁
+                //当前线程即使没有持有锁，但还是调用了释放锁的方法
                 "local mode = redis.call('hget', KEYS[1], 'mode'); " +
                 "if (mode == false) then " +
+                        //如果锁模式为空，往读写锁对应的channel发送释放锁的消息，然后返回1，lua脚本执行完毕
+                        //channel 发布锁释放消息的用处：
+                        //其实在 Redisson 提供的各种分布式锁中，不管是可重入锁、公平锁，还是到现在的读写锁，都会利用 redis 的pub/sub机制来做下面的通知机制。
+                        //在线程获取锁失败的时候，在等待前会先订阅锁对应的 channel，然后进入等待状态。
+                        //如果当前线程成功释放锁，那么会在锁对应的 channel 发布释放锁的消息；假设此时有其他线程在等待获取锁，那么就会接收到 channel 里释放锁的消息，提前跳出等待状态，去获取锁。
+                        //关于锁channel，要注意的是：可重入锁和读写锁，等待线程都是订阅同一个channel；而公平锁不是，公平锁是每个等待线程都订阅自己指定的channel，从而做到公平。
                     "redis.call('publish', KEYS[2], ARGV[1]); " +
                     "return 1; " +
                 "end; " +
+                        //分支二：锁存在，但当前线程没有持有锁
                 "local lockExists = redis.call('hexists', KEYS[1], ARGV[2]); " +
+                        //利用 hexists 命令判断当前线程是否持有锁
                 "if (lockExists == 0) then " +
+                        //如果不存在直接返回null，表示释放锁失败
                     "return nil;" +
                 "end; " +
-                    
-                "local counter = redis.call('hincrby', KEYS[1], ARGV[2], -1); " + 
+                    //分支三：锁存在且当前线程持有锁
+                    //利用 hincrby 命令，给当前线程持有锁数量减1
+                "local counter = redis.call('hincrby', KEYS[1], ARGV[2], -1); " +
+                        //如果持有锁数量减1后等于0(注意当前线程重入是不会进入这个条件的)，证明当前线程不再持有锁，那么利用 hdel 命令将锁map中加锁次数记录删掉
                 "if (counter == 0) then " +
                     "redis.call('hdel', KEYS[1], ARGV[2]); " + 
                 "end;" +
+                        //删除线程持有锁对应的加锁超时记录
+                        //为什么给读锁扣减不需要先判断锁的模式？在锁map中记录加锁次数时，读锁的key是UUID:threadId，即客户端ID:线程ID；而写锁的key是UUID:threadId:write，即客户端ID:线程ID:write，那么就是说读锁的key和写锁的key是不一样的。所以解锁的时候，直接使用对应key来扣减持有锁次数即可。
+                        //还有一点很重要的是，相同线程，如果获取了写锁后，还是可以继续获取读锁的。所以只需要判断锁map有读锁加锁次数记录即可，就可以判断当前线程是持有读锁的，并不需要关心当前锁的模式。
                 "redis.call('del', KEYS[3] .. ':' .. (counter+1)); " +
-                
+                //分支四：给当前锁刷新过期时间
+                        //当前线程释放一次读锁后，锁map还存在加锁次数记录
+                        //有可能是当前线程还持有读锁/写锁，或者当前线程或其他线程同时持有读锁
+                        //利用 hlen 获取锁map中key的数量
+                        //如果锁map中 key 的数量还是大于1，那么证明还有线程持有锁，遍历锁map集合中的加锁次数key，根据加锁超时记录获取最大的超时时间
                 "if (redis.call('hlen', KEYS[1]) > 1) then " +
-                    "local maxRemainTime = -3; " + 
-                    "local keys = redis.call('hkeys', KEYS[1]); " + 
-                    "for n, key in ipairs(keys) do " + 
+                        //设置 maxRemainTime 为 -3
+                    "local maxRemainTime = -3; " +
+                        //利用 hkeys 命令获取锁map中所有key
+                    "local keys = redis.call('hkeys', KEYS[1]); " +
+                        //遍历
+                    "for n, key in ipairs(keys) do " +
+                        //利用 hget 命令获取key对应的加锁次数
                         "counter = tonumber(redis.call('hget', KEYS[1], key)); " + 
-                        "if type(counter) == 'number' then " + 
-                            "for i=counter, 1, -1 do " + 
-                                "local remainTime = redis.call('pttl', KEYS[4] .. ':' .. key .. ':rwlock_timeout:' .. i); " + 
+                        "if type(counter) == 'number' then " +
+                            //遍历加锁次数
+                            "for i=counter, 1, -1 do " +
+                                //拼接 key 对应的加锁记录对应的超时时间，利用 pttl 获取超时时间
+                                "local remainTime = redis.call('pttl', KEYS[4] .. ':' .. key .. ':rwlock_timeout:' .. i); " +
+                                //与 maxRemainTime 对比，获取当前最大的超时时间，赋值给 maxRemainTime
                                 "maxRemainTime = math.max(remainTime, maxRemainTime);" + 
                             "end; " + 
                         "end; " + 
                     "end; " +
-                            
+                            //判断 maxRemainTime 是否大于0，如果大于0，给锁重新设置过期时间为 maxRemainTime，然后返回0结束lua脚本的执行
                     "if maxRemainTime > 0 then " +
                         "redis.call('pexpire', KEYS[1], maxRemainTime); " +
                         "return 0; " +
                     "end;" + 
-                        
+                        //如果当前读写锁的锁模式是写锁，直接返回0结束lua脚本的执行
                     "if mode == 'write' then " + 
                         "return 0;" + 
                     "end; " +
                 "end; " +
-                    
+                    //当走到最后的操作，证明当前线程不但成功释放锁，并且释放后当前读写锁已经没有其他线程再持有锁了
+                    //所以到这里，我们可以直接将读写锁对应的key直接删掉，并且往读写锁对应的channel中发布释放锁消息
                 "redis.call('del', KEYS[1]); " +
                 "redis.call('publish', KEYS[2], ARGV[1]); " +
                 "return 1; ",
